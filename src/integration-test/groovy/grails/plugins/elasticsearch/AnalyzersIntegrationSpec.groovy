1d7c5faddb57414d27cff8f676db04ddec1a8bf1
James Kleeh <james.kleeh@gmail.com>
2016-02-04 12:23:50 -0500

    Changes from pbehl's PR

--- src/integration-test/groovy/grails/plugins/elasticsearch/AnalyzersIntegrationSpec.groovy
+++ src/integration-test/groovy/grails/plugins/elasticsearch/AnalyzersIntegrationSpec.groovy
@@ -20,33 +20,26 @@ class AnalyzersIntegrationSpec extends Specification {
     @Autowired
     ElasticSearchService elasticSearchService
 
-    @Shared
-            posts = []
-
-    def setupSpec() {
-        Post.withNewSession {
-
-            posts << new Post(subject: "[abc] Grails 3.0 M1 Released!",
-                    body: "Grails 3.0 milestone 1 is now available.").save(failOnError: true)
-            posts << new Post(subject: "The Future of Groovy and Grails Sponsorship",
-                    body: "[abc] http://grails.io/post/108534902333/the-future-of-groovy-grails-sponsorship").save(failOnError: true)
-            posts << new Post(subject: "GORM for MongoDB 3.0 Released",
-                    body: "[xyz] GORM for MongoDB 3.0 has been released with support for MongoDB 2.6 features, including the " +
+    def setup() {
+        new Post(subject: "[abc] Grails 3.0 M1 Released!",
+                 body: "Grails 3.0 milestone 1 is now available.").save(failOnError: true)
+        new Post(subject: "The Future of Groovy and Grails Sponsorship",
+                 body: "[abc] http://grails.io/post/108534902333/the-future-of-groovy-grails-sponsorship").save(failOnError: true)
+        new Post(subject: "GORM for MongoDB 3.0 Released",
+                 body: "[xyz] GORM for MongoDB 3.0 has been released with support for MongoDB 2.6 features, including the " +
                             "new GeoJSON types and full text search.").save(failOnError: true)
-        }
     }
 
-    def cleanupSpec() {
-        Post.withNewSession {
-            posts.each { Post post ->
-                post.delete()
-            }
+    def cleanup() {
+        Post.all.each { Post post ->
+            post.delete()
         }
     }
 
     def "search by all"() {
         given:
         elasticSearchAdminService.refresh()
+
         expect:
         elasticSearchService.search('xyz', [indices: Post, types: Post]).total == 3
 
@@ -60,6 +53,7 @@ class AnalyzersIntegrationSpec extends Specification {
     def "search by subject"() {
         given:
         elasticSearchAdminService.refresh()
+
         expect:
         elasticSearchService.search([indices: Post, types: Post], QueryBuilders.matchQuery('subject', 'xyz')).total == 0
 
